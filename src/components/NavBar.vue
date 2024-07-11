12378213b86b974abace476c3a597223dbb1a39d
shengnan <pelevin1111@gmail.com>
2024-07-12 02:07:39 +0300

    Update navbar menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
+import { computed, ref, shallowRef, watchEffect } from 'vue'
 
+
+const windowWidth = ref(window.innerWidth)
+
+watchEffect(() => {
+      const updateWindowSize = () => {
+        windowWidth.value = window.innerWidth;
+      };
+      window.addEventListener('resize', updateWindowSize);
+      return () => window.removeEventListener('resize', updateWindowSize);
+});
 </script>
 
 <template>
   <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+    <div class="max-w-screen-xxl flex flex-wrap items-center justify-between mx-auto p-4">
+      <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
         <img src="../assets/img/favicon.svg" class="h-8" alt="SSA Logo">
-        <span
-          class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Система Стриминга Автомобилей</span>
+        <span v-if="windowWidth > 560" class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Система Стриминга Автомобилей</span>
+        <span v-else class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">ССА</span>
       </a>
       <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
         <button type="button"
