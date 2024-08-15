a80248eb62d1742edd64923cc7f1a3c43c1b0d77
shengnan <pelevin1111@gmail.com>
2024-08-15 18:05:39 +0300

    add keycloak

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,10 +4,21 @@ import { createPinia } from 'pinia'
 import './main.css'
 import App from './App.vue'
 import { router } from '@/routes/router'
+import { vueKeycloak } from '@josempgon/vue-keycloak'
 
 
-const app = createApp(App)
+(async () => {
+  const app = createApp(App)
+  await vueKeycloak.install(app, {
+    config: {
+      url: `http://127.0.0.1:9080/`,
+      realm: 'master',
+      clientId: 'vue_client'
+    }
+  })
+  app.use(createPinia())
+  app.use(router)
+  app.mount('#app')
+})()
 
-app.use(createPinia())
-app.use(router)
-app.mount('#app')
+
