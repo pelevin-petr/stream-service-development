696f7e1d064887fd56693cd6a0fb37b53b0ddf89
shengnan <pelevin1111@gmail.com>
2024-08-28 00:32:54 +0300

    fix nav

--- frontend/src/components/common/header/HeaderComponent.vue
+++ frontend/src/components/common/header/HeaderComponent.vue
@@ -10,20 +10,24 @@ const { isAuthenticated } = useKeycloak()
 const visible = ref(window.innerWidth >= 1280)
 const headerClass = ref(window.innerWidth >= 1280)
 const { keycloak } = useKeycloak()
-
 </script>
 
 <template>
   <nav
     class="bg-white dark:bg-extra-neutral-700 border-b border-gray-200 dark:border-gray-600">
-    <div class="grid grid-cols-3  mx-auto p-4"
+    <div class="grid mx-auto p-4"
          :class="{'grid-cols-3': headerClass, 'grid-cols-2': !headerClass}"
     >
-      <RouterLink class="flex items-center space-x-3 rtl:space-x-reverse" to="/">
+      <RouterLink
+        class="flex items-center space-x-3 rtl:space-x-reverse"
+        to="/"
+        @click="visible = !visible"
+      >
         <img alt="SSA Logo" class="h-8" :src="faviconSVG">
         <span class=" hidden md:block self-center text-xl font-semibold whitespace-nowrap dark:text-white">Система Стриминга Автомобилей</span>
         <span class=" block md:hidden self-center text-xl font-semibold whitespace-nowrap dark:text-white">ССА</span>
       </RouterLink>
+      
       <div class="flex justify-end xl:order-2 space-x-3 xl:space-x-0 rtl:space-x-reverse">
         <div v-if="!isAuthenticated">
           <span @click="keycloak?.login">
@@ -35,13 +39,13 @@ const { keycloak } = useKeycloak()
           </span>
         </div>
         <div v-else
-         class="my-auto font-medium text-green-600 bg-green-200 py-1 px-2 rounded-lg border border-green-600"
+             class="my-auto font-medium text-green-600 bg-green-200 py-1 px-2 rounded-lg border border-green-600"
         >
           Вы авторизованы
         </div>
         <button @click="visible = !visible"
                 aria-controls="navbar-sticky" aria-expanded="false"
-                class="inline-flex justify-center  items-center p-2 w-10 h-10  text-sm text-gray-500 rounded-lg xl:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                class="inline-flex justify-center items-center p-2 w-10 h-10  text-sm text-gray-500 rounded-lg xl:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                 data-collapse-toggle="navbar-sticky" type="button">
           <span class="sr-only">Open main menu</span>
           <svg aria-hidden="true" class="w-5 h-5" fill="none" viewBox="0 0 17 14" xmlns="http://www.w3.org/2000/svg">
@@ -51,13 +55,11 @@ const { keycloak } = useKeycloak()
         </button>
       </div>
       <div class="flex justify-center" :class="{'col-span-full': !headerClass}">
-        <NavBar v-show="visible">
+        <NavBar v-show="visible" v-model="visible">
         </NavBar>
       </div>
     </div>
   </nav>
-
-
 </template>
 
 <style scoped>
--- frontend/src/components/common/header/NavBar.vue
+++ frontend/src/components/common/header/NavBar.vue
@@ -3,15 +3,17 @@ import { useKeycloak } from '@josempgon/vue-keycloak'
 
 
 const { isAuthenticated } = useKeycloak()
+const visible = defineModel()
 </script>
 <template>
-  <div class="items-center justify-between w-full xl:flex xl:w-auto xl:order-1 ">
+  <div class="w-full">
     <ul
-      class="flex items-center justify-between p-4 xl:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 dark:bg-extra-neutral-700 xl:space-x-8 rtl:space-x-reverse xl:flex-row xl:mt-0 xl:border-0 xl:bg-white dark:bg-gray-800 xl:dark:bg-gray-900 dark:border-gray-700">
+      class="flex flex-col justify-between p-4 xl:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 dark:bg-extra-neutral-700 xl:space-x-8 rtl:space-x-reverse xl:flex-row xl:mt-0 xl:border-0 xl:bg-white dark:bg-gray-800 xl:dark:bg-gray-900 dark:border-gray-700">
       <li class="mt-2">
         <RouterLink
           to="/"
           class="block py-2 px-3 text-white xl:text-gray-900 bg-blue-600 rounded xl:bg-transparent xl:hover:text-blue-700 dark:text-white dark:bg-extra-neutral-700 xl:p-0 "
+          @click="visible = !visible"
         >
           Главная
         </RouterLink>
@@ -20,6 +22,7 @@ const { isAuthenticated } = useKeycloak()
         <RouterLink
           to="/instructors"
           class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 xl:hover:bg-transparent xl:hover:text-blue-700 xl:p-0 lg:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white xl:dark:hover:bg-transparent dark:border-gray-700"
+          @click="visible = !visible"
         >
           Инструкторы
         </RouterLink>
@@ -28,6 +31,7 @@ const { isAuthenticated } = useKeycloak()
         <RouterLink
           to="/create-streams"
           class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 xl:hover:bg-transparent xl:hover:text-blue-700 xl:p-0 xl:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white xlZz:dark:hover:bg-transparent dark:border-gray-700"
+          @click="visible = !visible"
         >
           Стримы
         </RouterLink>
