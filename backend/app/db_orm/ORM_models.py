358dc9478611912fd6638728aef0856ab29ac12d
shengnan <pelevin1111@gmail.com>
2024-08-29 12:34:04 +0300

    add instructors table

--- backend/app/db_orm/ORM_models.py
+++ backend/app/db_orm/ORM_models.py
@@ -1,9 +1,11 @@
-from sqlalchemy import Integer, MetaData
+from sqlalchemy import Integer, MetaData, String
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+
+metadata_obj = MetaData()
 
 
 class Base(DeclarativeBase):
-    pass
+    metadata = metadata_obj
 
     repr_cols_num = 3  # Надо будет увеличить, когда будет больше столбцов
     repr_cols = tuple()
@@ -17,4 +19,11 @@ class StreamsOrm(Base):
     description: Mapped[str]
 
 
-metadata_obj = MetaData()
+class InstructorsOrm(Base):
+    __tablename__ = "instructors_service_data_test_ORM"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    fullname: Mapped[str]
+    work_experience: Mapped[str]
+
+
