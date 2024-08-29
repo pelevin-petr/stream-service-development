775c006f08104451274f85ee4780953b0d407ea6
shengnan <pelevin1111@gmail.com>
2024-08-29 12:33:41 +0300

    init

---
+++ backend/app/models/instructor.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field, ConfigDict
+
+
+class BaseInstructor(BaseModel):
+    fullname: str = Field(min_length=1, max_length=25)
+    work_experience: str = Field(min_length=0, max_length=100)
+
+
+class CreateInstructor(BaseInstructor):
+    pass
+
+
+class Instructor(BaseInstructor):
+    id: int
+
+    model_config = ConfigDict(from_attributes=True)
