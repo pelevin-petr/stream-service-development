6a57c5c682d466118e80ec33d0d81e2366ead769
shengnan <pelevin1111@gmail.com>
2024-09-03 01:19:52 +0300

    fix fullname max length

--- backend/app/models/instructor.py
+++ backend/app/models/instructor.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field, ConfigDict
 
 
 class BaseInstructor(BaseModel):
-    fullname: str = Field(min_length=1, max_length=25)
+    fullname: str = Field(min_length=1, max_length=50)
     work_experience: str = Field(min_length=0, max_length=100)
 
 
