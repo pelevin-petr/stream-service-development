4330c7a7c98924eea7176c1df0f7d8969f981c90
shengnan <pelevin1111@gmail.com>
2024-09-03 01:15:02 +0300

    add nav

--- frontend/src/admin/views/AdminView.vue
+++ frontend/src/admin/views/AdminView.vue
@@ -1,12 +1,27 @@
 <script lang="ts" setup>
 import FooterComponent from '@/shared/components/common/footer/FooterComponent.vue'
 import HeaderComponent from '@/shared/components/common/header/HeaderComponent.vue'
-</script>
 
+const nav = [
+  {
+    to: '/',
+    title: 'Главная'
+  },
+  {
+    to: '/admin/instructors',
+    title: 'Инстукторы'
+  },
+  {
+    to: '/admin/streams',
+    title: 'Стримы'
+  }
+]
+
+</script>
 <template>
   <div class="flex flex-col min-h-full justify-between">
     <div class="flex flex-col items-center">
-     <HeaderComponent class="w-full" />
+     <HeaderComponent class="w-full" :routes="nav"/>
       <RouterView class="dark:bg-gray-800 mt-8 md:px-8 w-full"/>
     </div>
     
