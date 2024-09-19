00081edc4e0849da61bc8a6c9cf58c8d0a09c498
shengnan <pelevin1111@gmail.com>
2024-09-19 16:26:45 +0300

    add file(img) in model Stream

--- backend/app/models/stream.py
+++ backend/app/models/stream.py
@@ -1,9 +1,13 @@
+from typing import Optional
+
+from fastapi import UploadFile
 from pydantic import BaseModel, Field, ConfigDict
 
 
-class BaseStream(BaseModel):
-    title: str = Field(min_length=1, max_length=25)
+class BaseStream(BaseModel): 
+    title: str = Field(min_length=1, max_length=50)
     description: str = Field(min_length=0, max_length=100)
+    # file: bytes = Field(min_length=0)
 
 
 class CreateStream(BaseStream):
