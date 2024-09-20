1a69cc7083fb42a89be621e6b2330c6342226856
shengnan <pelevin1111@gmail.com>
2024-09-20 19:22:41 +0300

    last commit

--- frontend/src/default/views/TopView.vue
+++ frontend/src/default/views/TopView.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 import FooterComponent from '@/shared/components/common/footer/FooterComponent.vue'
 import HeaderComponent from '@/shared/components/common/header/HeaderComponent.vue'
+
+
 const nav = [
   {
     to: '/',
@@ -9,20 +11,17 @@ const nav = [
   {
     to: '/instructors',
     title: 'Инстукторы'
-  },
-  {
-    to: '/streams',
-    title: 'Стримы'
   }
 ]
+
 
 </script>
 
 <template>
   <div class="flex flex-col min-h-full justify-between">
     <div class="flex flex-col items-center">
-     <HeaderComponent class="w-full" :routes="nav"/>
-      <RouterView class="dark:bg-gray-800 mt-8 md:px-8 w-full"/>
+      <HeaderComponent class="w-full" :routes="nav" />
+      <RouterView class="dark:bg-gray-800 mt-8 w-full" />
     </div>
     
     <FooterComponent class="mt-8" />
